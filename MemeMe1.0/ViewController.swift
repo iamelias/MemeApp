648373b40bc5462ed3bba0f482d117c9315bd046
Elias Hall <elias.hall@gmail.com>
2019-06-26 22:46:17 -0500

    implemented camera use, made camera button set to disabled if camera use is not possible

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -10,20 +10,34 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         
 
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera) //disabling camera button if camera isn't available
 
+    }
+    
     @IBAction func pickAlbumImage(_ sender: Any) { //opens album for image selection
         let pickImage = UIImagePickerController()
         pickImage.delegate = self
+        pickImage.sourceType = .photoLibrary //using sourceType property to distinguish between album and camera
         present(pickImage, animated: true, completion: nil)
         
  
         }
+    @IBAction func pickImageCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
         
       func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
